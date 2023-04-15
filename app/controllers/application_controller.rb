85290d4f852b581f20f1abbb4eb43cd2ec1f09a7
Kiara Jeff <ki.mij@icloud.com>
2023-04-15 08:28:00 +0000

    added params an finished first form....added math

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
+
+  def calculate_square
+    # params = {"entry=42"}
+
+    @num = params.fetch("entry").to_i
+
+    @square_of_num = @num ** 2
+
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 end
