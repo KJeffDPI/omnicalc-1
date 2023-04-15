7c288f80a23d900e169f0cf9760abf9db9912402
Kiara Jeff <ki.mij@icloud.com>
2023-04-15 20:34:49 +0000

    finished! added calculating payments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,25 @@ class ApplicationController < ActionController::Base
 
     @square_root_of_number = Math.sqrt(@number)
 
-    render({ :template => "calculation_templates/square_root_results.html.erb"})
+    render({ :template => "calculation_templates/square_root_results.html.erb" })
   end
 
+  def blank_payment_form
+
+    render({ :template => "calculation_templates/payment_form.html.erb" })    
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_f
+    @principal = params.fetch("user_pv").to_f
+
+    n = @years * 12
+    r = @apr / 100 / 12
+    numerator = r * @principal
+    denominator = 1 - (1 + r) ** -n
+    @payment = numerator / denominator
+
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ get("/square_root/new", { :controller => "application", :action => "blank_square
 
 get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
 
-#get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
+get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
 
-#get("/payment/results", { :controller => "application", :action => "calculate_payment" })
+get("/payment/results", { :controller => "application", :action => "calculate_payment" })
 end
